1bd54c301e26b99e40e8cad098c8d36fafff04ba
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2021-08-18 19:07:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let mobilemenu = document.querySelector(".mobile-menu")
         mobilemenu.addEventListener("click", clicar)
         function clicar(){
-            let menu = document.querySelector('.menu')
+            let menu = document.querySelector('.container')
             menu.classList.toggle('show-menu')
         }
